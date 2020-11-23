14773bb6d80a4f8d9d119bdd547f1265e78c0400
Ryan MacFarlane <rjl.macfarlane@gmail.com>
2020-11-23 20:17:38 +0000

    Completed comparative function.

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
