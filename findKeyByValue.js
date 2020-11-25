02f7665ffc5d77a34728bb268edf8b250e6343d7
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-11-25 16:52:48 -0400

    Completed findByKeyValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(obj, value) {
+  for (const key of Object.keys(obj)) {
+    if (value === obj[key]) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
