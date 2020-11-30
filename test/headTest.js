ee7d76bcef02439693884fe6b6884a87091159c2
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-11-30 10:40:22 -0400

    Added require/export statements to tail.js and corresponding tester file

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,8 @@
+const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 // TEST CODE
-head("Lighthouse Labs", "Bootcamp"); // Should fail
-head(1, 1); // Should pass
-head(([5,6,7]), 5); // Should pass
-head((["Hello", "Lighthouse", "Labs"]), "Hello"); // Should pass
+assertEqual("Lighthouse Labs", "Bootcamp"); // Should fail
+assertEqual(1, 1); // Should pass
+assertEqual(head([5,6,7]), 5); // Should pass
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Should pass
