f37f9a40b89ad02eb0dda623e650797f77d977a2
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-11-30 10:45:49 -0400

    Added require/export statements to eqArrays and corresponding test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = function(a, b) {
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i] || a.length !== b.length) {
@@ -12,8 +6,4 @@ const eqArrays = function(a, b) {
   } return true;
 };
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
