320346bc32244ded6aaa72d49685a6e4fe3365e1
rjlmacfarlane <rjl.macfarlane@gmail.com>
2020-11-30 15:39:42 -0400

    Added M&C tests for middleTest.js, removed primitive test code.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,15 @@
+const { assert } = require('chai');
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
-// TEST CODE
-console.log(assertArraysEqual(middle([1, 2]), [])); // Prints [] => Passes (because there is no middle in the first array).
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [ 5, 6 ])); // Prints [ 5, 6 ] => Passes
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [ 5 ])); // Prints [ 5 ] => Passes
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [ 9 ])); // Prints [ 5 ] => Fails
+describe("#middle", () => {
+  it("Returns an empty array when array length is <= 2", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("Returns the middle two numbers of an even-length array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [ 5, 6 ]);
+  });
+  it("Returns the middle number of an odd-length array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [ 5 ]);
+  });
+});
